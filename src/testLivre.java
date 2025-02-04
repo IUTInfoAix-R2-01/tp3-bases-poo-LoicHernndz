4a45e958feaca9196912801b2c1778662ba1b5dc
Loic HERNANDEZ <loic.hernandez@etu.univ-amu.fr>
2025-02-04 14:10:07 +0100

    Ajout de la classe Livre

---
+++ src/testLivre.java
@@ -0,0 +1,11 @@
+
+public class testLivre {
+	public static void main(String[] args) {
+		Livre livre = new Livre ("Le Livre", 17.99f, 7);
+		
+		System.out.println(livre);
+		System.out.println("Nom : " + livre.getName());
+		System.out.println("Prix : " + livre.getPrice()+"€");
+		System.out.println("Quantite : " + livre.getQuantite());
+	}
+}
